00e5ede0f244ea2048f7de1910cc088b8506dd8f
Diogo Silva <56855879+Diogojackson@users.noreply.github.com>
2024-04-09 12:20:56 +1000

    update

--- R/get_species.R
+++ R/get_species.R
@@ -1,4 +1,4 @@
-get_species <- function(species = c()) {
+get_species <- function(species = c(), place = c("Brisbane", "Cairns", "Sydney")) {
   # Lista de espécies e seus URLs
   species_urls <- list(
     "Delia_cristata" = "https://datadryad.org/stash/downloads/file_stream/3048006",
@@ -7,28 +7,35 @@ get_species <- function(species = c()) {
     "Leptuca_albugularis" = "https://datadryad.org/stash/downloads/file_stream/3048005"
     # Adicione mais espécies conforme necessário
   )
-  
+
+  place_list <- list(
+    "Brisbane",
+    "Cairns",
+    "Sydney"
+    # Adicione mais lugares conforme necessário
+  )
   # Criação da pasta "species" para os downloads
   species_folder <- "butterfly_data_species"
   dir.create(species_folder, showWarnings = FALSE)
-  
+
   for (sp in species) {
     if (sp %in% names(species_urls)) {
       # Nome do arquivo ZIP
       zip_file <- paste0(sp, ".zip")
       # Caminho completo para o arquivo ZIP
       zip_path <- file.path(species_folder, zip_file)
-      
+
       # Download do arquivo ZIP
       download.file(url = species_urls[[sp]], destfile = zip_path, mode = "wb")
-      
+
       # Descompactar o arquivo ZIP
       unzip(zip_path, exdir = file.path(species_folder, sp))
-      
-      cat("Espécie", sp, "baixada com sucesso!\n")
+
+      cat("Espécie", sp, "baixada com sucesso no diretorio:\n", getwd())
     } else {
       cat("Espécie", sp, "não encontrada!\n")
     }
   }
 }
 
+get_species(species = c("Delia_cristata", "Amata_leptodactyla"), place = "Brisbane")
